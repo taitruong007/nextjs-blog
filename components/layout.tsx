a84c533ee14bff681e8b70e2d18f3187abdde66d
Tai Truong <tai.truong@andpad.co.jp>
2022-11-17 15:35:18 +0700

    test dependency cruiser

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,22 @@
+import { ReactNode } from 'react';
+
+import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image';
+
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
 
 const name = 'Tai Truong';
 export const siteTitle = 'Next.js Sample Website';
 
-export default function Layout({ children, home }) {
+export default function Layout({
+  children,
+  home,
+}: {
+  children: ReactNode;
+  home?: boolean;
+}) {
   return (
     <div className={styles.container}>
       <Head>
