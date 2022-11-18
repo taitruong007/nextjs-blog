3963ae669989e51cb93212af963e6952495e7133
Tai Truong <tai.truong@andpad.co.jp>
2022-11-18 14:33:47 +0700

    test custom action clone

---
+++ .github/actions/action-dependency-cruiser/src/main.ts
@@ -0,0 +1,22 @@
+import * as core from '@actions/core'
+import { getOctokit } from '@actions/github'
+
+import { ActionError, exitWithMessage } from './ActionError'
+import { installDependencies } from './installDependencies'
+import { getOptions } from './options'
+import { generateReport } from './report/generateReport'
+import { runDepcruise } from './runDepcruise'
+
+export const run = async (): Promise<void> => {
+  const options = await getOptions()
+  const octokit = getOctokit(options.token)
+
+  await installDependencies()
+  const { mermaidText, cmdText } = await runDepcruise(options)
+  await generateReport(octokit, options, mermaidText, cmdText)
+}
+
+run().catch((error) => {
+  if (error instanceof ActionError) exitWithMessage(error)
+  else if (error instanceof Error) core.setFailed(error.message)
+})
